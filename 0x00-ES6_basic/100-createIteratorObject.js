b0d474f0f31b2dda78bbf42acfd2a036a94a4949
Dru Mumba <drusademumba@gmail.com>
2024-03-04 20:38:48 +0300

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (const val of Object.values(report.allEmployees)) {
+    employees = [...employees, ...val];
+  }
+
+  return employees;
+}
