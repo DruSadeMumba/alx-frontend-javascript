d08c8d6473fa76292bc4494533b572f73ee6ea2a
Dru Mumba <drusademumba@gmail.com>
2024-03-05 17:33:33 +0300

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,12 @@
 export default function getResponseFromAPI() {
-
+  return new Promise((resolve, reject) => {
+    let status;
+    setTimeout(() => {
+      if (status === 200) {
+        resolve('Success')
+      } else {
+        reject(Error(`Failed status: ${status}`))
+      }
+    }, 2000);
+  });
 }
