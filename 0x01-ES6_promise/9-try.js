5ea3d2b128e63119b582e150a4aa07c24712b6b6
Dru Mumba <drusademumba@gmail.com>
2024-03-06 14:37:51 +0300

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,11 @@
 export default function guardrail(mathFunction) {
-
+  const res = [];
+  try {
+    res.push(mathFunction());
+  } catch (err) {
+    res.push(err.message);
+  } finally {
+    res.push('Guardrail was processed');
+  }
+  return res;
 }
